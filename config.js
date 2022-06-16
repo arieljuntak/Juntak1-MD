5db1fc7f065267cb4ac35ad17b4c0f2aad62e02f
arieljuntak <107545690+arieljuntak@users.noreply.github.com>
2022-06-16 12:17:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6288708439726'],
+  ['6288708439726'],
+  ['6288708439726', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,7 +40,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
+  var sticker_name = '@juntak_bot'
   var sticker_author = 'Aine'
 } else {
   var sticker_name = stickerpack.spackname
